1733e85310f98917230593f37ef4529f6f463ce1
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-09-14 03:04:50 +0700

    chore: add Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
     content: ["./src/**/*.{njk,html}"],
     theme: {
         extend: {
+            colors: {
+                light: {
+                    bg: {
+                        default: "#FFFFFF",
+                    },
+                    label: {
+                        default: "#000000",
+                        secondary: "rgba(60,60,67,0.6)",
+                    },
+                },
+            },
             fontFamily: {
                 inter: ["Inter", "sans-serif"],
             },
